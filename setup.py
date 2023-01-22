fe0c75c470f93ffa7e14d81ac8afd64090df0b3e
hiddify <hiddify@gmail.com>
2023-01-22 16:29:27 +0100

    include static files

--- setup.py
+++ setup.py
@@ -37,6 +37,7 @@ setup(
     long_description=read("README.md"),
     long_description_content_type="text/markdown",
     author="hiddify",
+    include_package_data=True,
     packages=find_packages(exclude=["tests", ".github"]),
     install_requires=read_requirements("requirements.txt"),
     entry_points={
