cde79a16dbeb1730dc4ce723fccb99a700b00e19
hiddify <hiddify@gmail.com>
2023-02-02 15:44:13 +0100

    add auto translate

---
+++ auto_translate.py
@@ -0,0 +1,32 @@
+import polib
+from deep_translator import GoogleTranslator
+import sys
+if __name__ == "__main__":  
+    src=sys.argv[1]
+    dst=sys.argv[2]
+    src_file=f'hiddifypanel/translations/{src}/LC_MESSAGES/messages.po'
+    dst_file=f'hiddifypanel/translations/{dst}/LC_MESSAGES/messages.po'
+    src_pofile = polib.pofile(src_file)
+    dst_pofile = polib.pofile(dst_file)
+    translator = GoogleTranslator(source=src, target=dst if dst !='zh' else "zh-CN")
+    
+
+    for entry in dst_pofile:
+        if "incomplete" in entry.flags:
+            entry.flags.remove("incomplete")
+
+        if "auto_translate" not in entry.flags and ('fuzzy' in entry.flags or entry.msgstr==""):
+            entry.msgstr=""
+            if "fuzzy" in entry.flags:
+                entry.flags.remove("fuzzy")
+
+            src_prof=src_pofile.find(entry.msgid)    
+            if src_prof and src_prof.msgstr and 'fuzzy' not in src_prof.flags:
+                entry.msgstr=translator.translate(src_prof.msgstr) 
+                
+                print(src_prof.msgid, src_prof.msgstr,entry.msgstr)
+                entry.flags.append("auto_translate")
+
+            
+
+    dst_pofile.save(dst_file)
--- update_translations.sh
+++ update_translations.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 PATH=/config/.local/bin:$PATH
 pybabel extract -F babel.cfg -o messages.pot hiddifypanel
-pybabel update  -i messages.pot -d hiddifypanel/translations -l fa
 pybabel update  -i messages.pot -d hiddifypanel/translations -l en
+
+pip install polib deep-translator
+python3 auto_translate.py fa en
+python3 auto_translate.py en fa
+python3 auto_translate.py en zh
+
+pybabel update  -i messages.pot -d hiddifypanel/translations -l fa
 pybabel update  -i messages.pot -d hiddifypanel/translations -l zh
 pybabel compile -f -d hiddifypanel/translations 
