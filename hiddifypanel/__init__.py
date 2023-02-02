f074dd09d5ae40d49de20cf0a5c1c65f877280a5
hiddify <hiddify@gmail.com>
2023-02-02 14:51:19 +0100

    fix

--- hiddifypanel/__init__.py
+++ hiddifypanel/__init__.py
@@ -2,5 +2,5 @@ from .base import create_app, create_app_wsgi
 from . import panel
 __all__ = ["create_app", "create_app_wsgi"]
 
-application = create_app_wsgi()
+# application = create_app_wsgi()
 
