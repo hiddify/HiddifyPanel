f25f3d5e5f0acc054e6e24cfefc215cc885cbc37
hiddify <hiddify@gmail.com>
2023-02-04 10:29:39 +0100

    full

--- hiddifypanel/panel/hiddify.py
+++ hiddifypanel/panel/hiddify.py
@@ -134,8 +134,8 @@ def get_available_proxies():
 
 
 def flash_config_success(full_install=False):
-    url='admin.Actions:'+('reinstall' if full_install else 'apply_configs')
-    apply_btn=f"<a href='{url_for('{url}')}' class='btn btn-primary'>"+_("admin.config.apply_configs")+"</a>"
+    url=url_for('admin.Actions:'+('reinstall' if full_install else 'apply_configs'))
+    apply_btn=f"<a href='{url}' class='btn btn-primary'>"+_("admin.config.apply_configs")+"</a>"
     flash(Markup(_('config.validation-success',link=apply_btn)), 'success')
 
 # Importing socket library 
