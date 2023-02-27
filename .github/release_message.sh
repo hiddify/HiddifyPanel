7b53db8f521d6b1e8af864665339a2fd7e8b6fd4
hiddify <hiddify@gmail.com>
2023-02-27 23:58:02 +0100

    fix: relase message bug

--- .github/release_message.sh
+++ .github/release_message.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 previous_release=$(curl --silent "https://api.github.com/repos/hiddify/HiddifyPanel/releases/latest" | grep -Po '"tag_name": "\K.*?(?=")')
 current=$(cat hiddifypanel/VERSION)
-gitchangelog "${previous_release}..v$current"
+gitchangelog "${previous_release}..$current"
