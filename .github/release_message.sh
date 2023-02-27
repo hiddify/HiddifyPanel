80c8bf6bebb2c9715f843ff3d87a4ae34d5630ea
hiddify <hiddify@gmail.com>
2023-02-27 23:52:53 +0100

    fix: version error

--- .github/release_message.sh
+++ .github/release_message.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 previous_release=$(curl --silent "https://api.github.com/repos/hiddify/HiddifyPanel/releases/latest" | grep -Po '"tag_name": "\K.*?(?=")')
-current=$(cat VERSION)
+current=$(cat hiddifypanel/VERSION)
 gitchangelog "${previous_release}..v$current"
