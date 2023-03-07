5c084e311b6e25f9fe93a0433a7eda71d44f0ddd
Ehsan <spin313@gmail.com>
2023-03-07 22:22:58 +0330

    File reorganized according to PEP8 principles for increasing code readability

--- hiddifypanel/models/user.py
+++ hiddifypanel/models/user.py
@@ -16,6 +16,7 @@ import uuid as uuid_mod
 from enum import auto
 from strenum import StrEnum
 
+
 class UserMode(StrEnum):
     no_reset = auto()
     monthly = auto()
@@ -25,18 +26,17 @@ class UserMode(StrEnum):
 
 
 class User(db.Model, SerializerMixin):
-    
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     uuid = db.Column(db.String(36), default=lambda: str(uuid_mod.uuid4()), nullable=False, unique=True)
     name = db.Column(db.String(512), nullable=False)
-    last_online=db.Column(db.DateTime,nullable=False,default=datetime.datetime.min)
+    last_online = db.Column(db.DateTime, nullable=False, default=datetime.datetime.min)
     expiry_time = db.Column(db.Date, default=datetime.date.today() + relativedelta.relativedelta(months=6))
     usage_limit_GB = db.Column(db.Numeric(6, 9, asdecimal=False), default=1000, nullable=False)
-    package_days=db.Column(db.Integer,default=90)
-    mode=db.Column(db.Enum(UserMode),default=UserMode.no_reset)
-    monthly=db.Column(db.Boolean,default=False)
+    package_days = db.Column(db.Integer, default=90)
+    mode = db.Column(db.Enum(UserMode), default=UserMode.no_reset)
+    monthly = db.Column(db.Boolean, default=False)
 
-    start_date=db.Column(db.Date,nullable=True)
+    start_date = db.Column(db.Date, nullable=True)
 
     current_usage_GB = db.Column(db.Numeric(6, 9, asdecimal=False), default=0, nullable=False)
     last_reset_time = db.Column(db.Date, default=datetime.date.today())
@@ -44,27 +44,30 @@ class User(db.Model, SerializerMixin):
 
 
 def is_user_active(u):
-    if u.mode==UserMode.disable:
+    if u.mode == UserMode.disable:
         return False
-    if u.usage_limit_GB<u.current_usage_GB:
+    if u.usage_limit_GB < u.current_usage_GB:
         return False
-    if remaining_days(u)<0:
+    if remaining_days(u) < 0:
         return False
     return True
-    
+
 
 def remaining_days(u):
     if u.start_date:
-        return u.package_days-(datetime.date.today()-u.start_date).days
+        return u.package_days - (datetime.date.today() - u.start_date).days
     return u.package_days
 
 
-package_mode_dic={
-        UserMode.daily:1,
-        UserMode.weekly:7,
-        UserMode.monthly:30
+package_mode_dic = {
+    UserMode.daily: 1,
+    UserMode.weekly: 7,
+    UserMode.monthly: 30
 }
+
+
 def days_to_reset(user):
     if user.start_date:
-        return package_mode_dic.get(user.mode,10000)-(datetime.date.today()-user.start_date).days % package_mode_dic.get(user.mode,10000)
-    return package_mode_dic.get(user.mode,10000)
+        return package_mode_dic.get(user.mode, 10000) - (
+                    datetime.date.today() - user.start_date).days % package_mode_dic.get(user.mode, 10000)
+    return package_mode_dic.get(user.mode, 10000)
