b24daa13c2f58f6ff43fa984add2bef6d82b9213
hiddify <hiddify@gmail.com>
2023-03-10 03:00:40 +0100

    fix: import

--- hiddifypanel/models/__init__.py
+++ hiddifypanel/models/__init__.py
@@ -2,7 +2,7 @@ from .config_enum import ConfigCategory,ConfigEnum
 from .config import StrConfig,BoolConfig,get_hconfigs,get_hdomains,hconfig,hdomain,set_hconfig
 
 from .domain import Domain,DomainType,ShowDomain,get_domain
-from .proxy import Proxy
+from .proxy import Proxy,ProxyL3,ProxyCDN,ProxyCDN,ProxyTransport
 from .user import User,UserMode,is_user_active,remaining_days,days_to_reset,user_by_id,user_by_uuid
 from .child import Child
 from .parent_domain import ParentDomain
