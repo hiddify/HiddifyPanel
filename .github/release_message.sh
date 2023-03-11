ee1dc9a5aaf4bb9f97a152b647ebc79d5ea0928a
hiddify <hiddify@gmail.com>
2023-03-11 22:28:28 +0100

    fix: release bug

--- .github/release_message.sh
+++ .github/release_message.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
-previous_release=$(curl --silent "https://api.github.com/repos/hiddify/HiddifyPanel/releases/latest" | grep -Po '"tag_name": "\K.*?(?=")')
+#previous_release=$(curl --silent "https://api.github.com/repos/hiddify/HiddifyPanel/releases/latest" | grep -Po '"tag_name": "\K.*?(?=")')
+previous_release=$(lastversion --at pip hiddifypanel)
 current=$(cat hiddifypanel/VERSION)
 gitchangelog "${previous_release}..$current"
