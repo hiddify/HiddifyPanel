1292e34278d6b17d44f71ed076f2c7dc007e4439
hiddify <hiddify@gmail.com>
2023-03-12 14:15:48 +0100

    test

--- release.sh
+++ release.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+if [ "$(id -u)" -ne 0 ]; then
+       echo 'This script must be run by root' >&2
+       exit 1
+fi
 source .env
 make release 
 cibuildwheel --platform linux --archs aarch64
