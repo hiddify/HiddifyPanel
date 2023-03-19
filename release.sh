ddea40d2da394e5ffa851af5bc6f0e66af3cb7a0
hiddify <hiddify@gmail.com>
2023-03-19 09:26:34 +0100

    fix: test

--- release.sh
+++ release.sh
@@ -6,20 +6,22 @@ if [ "$(id -u)" -ne 0 ]; then
        exit 1
 fi
 source .env
-make release 
+# make release 
 
 rm -rf build/; rm -rf dist/;
 rm -rf release
 cp -rf ./ release
 cd release 
-for f in "hiddifypanel/panel/hiddify.py" "hiddifypanel/panel/admin/UserAdmin.py" "hiddifypanel/panel/admin/ProxyDetailsAdmin.py" $(find hiddifypanel/panel/api/  hiddifypanel/models/ hiddifypanel/panel/telegrambot/ -name "*.py");do 
+
+for f in "hiddifypanel/panel/common.py" "hiddifypanel/panel/hiddify.py" "hiddifypanel/panel/admin/UserAdmin.py" "hiddifypanel/panel/admin/ProxyDetailsAdmin.py" $(find hiddifypanel/panel/api/  hiddifypanel/models/ hiddifypanel/panel/telegrambot/ -name "*.py");do 
     if [[ $(basename $f) != "__init__.py" ]];then
         mv $f ${f}x;
     fi
     echo "#واقعا برای 5 دلار میخوای کرک میکنی؟ حاجی ارزش وقت خودت بیشتره" > $(dirname $f)/read.py
     echo "#You want to crack it only for 5\$?" >> $(dirname $f)/read.py
 done
-
+# export CIBW_SKIP='py38 pp*'
+python3 setup.py  build_ext --inplace
 cibuildwheel --platform linux --archs aarch64
 # python3 setup.py bdist_wheel build_ext
 # exit 1
