76ff2ff86def20698727d1c5c11bbf16bedcdf8e
hiddify <hiddify@gmail.com>
2023-03-24 03:29:14 +0100

    new: add clean ip support

--- hiddifypanel/base2.py
+++ hiddifypanel/base2.py
@@ -4,11 +4,15 @@ import json
 import sys
 from datetime import datetime
 from flask import request
-
+from hiddifypanel.panel import hiddify,hiddify_api
+from hiddifypanel.models import *
 def init_app(app):
+    if hconfig(ConfigEnum.parent_panel):
+        hiddify_api.sync_child_to_parent()
     @app.before_request
     def test():
         print(request.base_url)
+        
     pass   
     # print(f"{app}")
     # print("Hello {}, the inference function has been successfully started".format(name))
