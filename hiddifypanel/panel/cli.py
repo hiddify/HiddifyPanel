9a98706b66f390fcdc877cef01176631c593465b
hiddify <hiddify@gmail.com>
2023-03-27 15:31:17 +0200

    fix: bug

--- hiddifypanel/panel/cli.py
+++ hiddifypanel/panel/cli.py
@@ -36,7 +36,7 @@ def all_configs():
     configs={
         "users": valid_users,
         "domains": [hiddify.domain_dict(u) for u in Domain.query.all()],
-        "parent_domains": [hiddify.parent_domain_dict(u) for u in ParentDomain.query.all()],
+        # "parent_domains": [hiddify.parent_domain_dict(u) for u in ParentDomain.query.all()],
         "hconfigs": get_hconfigs()
     }
     for d in configs['domains']:
