797f3f46667e17a6a2648d991a942288b50972d0
hiddify <hiddify@gmail.com>
2023-04-09 11:21:42 +0200

    fix: error in changelog

--- .github/release_message.sh
+++ .github/release_message.sh
@@ -3,4 +3,4 @@
 pip install lastversion >/dev/null 2>&1
 previous_release=$(lastversion --at pip hiddifypanel)
 current=$(cat hiddifypanel/VERSION)
-# gitchangelog "${previous_release}..$current"
+gitchangelog "${previous_release}..$current" ||echo "Error in change log"
