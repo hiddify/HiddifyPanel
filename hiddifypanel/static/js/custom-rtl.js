729205b74dcf36d2d94ab17c76590f41b4a54eea
hiddify <hiddify@gmail.com>
2023-04-10 21:54:43 +0200

    fix: persian ips

--- hiddifypanel/static/js/custom-rtl.js
+++ hiddifypanel/static/js/custom-rtl.js
@@ -2,7 +2,7 @@ function ConvertNumberToPersion() {
   let persian = { 0: '۰', 1: '۱', 2: '۲', 3: '۳', 4: '۴', 5: '۵', 6: '۶', 7: '۷', 8: '۸', 9: '۹' };
   function traverse(el) {
 //            console.log(el.tagName)
-    if (el.tagName=="PRE" || el.tagName=="STYLE" || el.tagName=="SCRIPT")return
+    if (el.tagName=="PRE" || el.tagName=="STYLE" || el.tagName=="SCRIPT" || el.tagName=="INPUT"|| el.tagName=="TEXTAREA")return
       if (el.nodeType == 3) {
           var list = el.data.match(/[0-9]/g);
           if (list != null && list.length != 0) {
