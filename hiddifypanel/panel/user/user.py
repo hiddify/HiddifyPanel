468ff3694f4d506f35465305d3883b1074b8329f
hiddify <hiddify@gmail.com>
2023-05-03 22:08:33 +0200

    fix: persian user profile

--- hiddifypanel/panel/user/user.py
+++ hiddifypanel/panel/user/user.py
@@ -100,7 +100,7 @@ class UserView(FlaskView):
             resp.headers['support-url']=hconfig(ConfigEnum.branding_site)
         resp.headers['profile-update-interval']=1
         # resp.headers['content-disposition']=f'attachment; filename="{c["db_domain"].alias or c["db_domain"].domain} {c["user"].name}"'
-        resp.headers['profile-title']=f'{c["db_domain"].alias or c["db_domain"].domain} {c["user"].name}'
+        resp.headers['profile-title']='base64:'+do_base_64(f'{c["db_domain"].alias or c["db_domain"].domain} {c["user"].name}')
 
         return resp
 
