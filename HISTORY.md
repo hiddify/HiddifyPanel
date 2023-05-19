15e559a45732c4aad9e7d5dc690f4e0fdf3a76b3
hiddify <hiddify@gmail.com>
2023-05-20 01:16:31 +0200

    release: version 7.0.0.dev13 🚀

--- HISTORY.md
+++ HISTORY.md
@@ -39,6 +39,8 @@
 
 #### Fix
 
+* Installation finished. 
+
 * Bug. 
 
 * Bug. 
--- hiddifypanel/VERSION.py
+++ hiddifypanel/VERSION.py
@@ -1,3 +1,3 @@
-__version__='7.0.0.dev12'
+__version__='7.0.0.dev13'
 from datetime import datetime
 __release_date__= datetime.strptime('2023-05-20','%Y-%m-%d')
