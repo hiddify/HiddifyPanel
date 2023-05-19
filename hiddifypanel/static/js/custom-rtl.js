897304c95b2b35f16f9bc06d2bacfd78ed6c2c8d
hiddify <hiddify@gmail.com>
2023-05-20 01:15:58 +0200

    fix: installation finished

--- hiddifypanel/static/js/custom-rtl.js
+++ hiddifypanel/static/js/custom-rtl.js
@@ -13,11 +13,15 @@ function ConvertNumberToPersion() {
           }
           
           var list = el.data.match(/[0-9]/g);
+          if(!(el.data.indexOf("http://")>=0 || el.data.indexOf("https://")>=0)){
+            
+          
           if (list != null && list.length != 0) {
               for (var i = 0; i < list.length; i++)
                   el.data = el.data.replace(list[i], persian[list[i]]);
                   
           }
+        }
       }
       for (var i = 0; i < el.childNodes.length; i++) {
           
