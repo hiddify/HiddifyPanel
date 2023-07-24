faafe131c96a0660227b040a2cc652526d19f937
hiddify <hiddify@gmail.com>
2023-07-24 06:05:49 +0200

    release: version 7.1.5 🚀

--- hiddifypanel/VERSION.py
+++ hiddifypanel/VERSION.py
@@ -1,3 +1,3 @@
-__version__='7.1.4'
+__version__='7.1.5'
 from datetime import datetime
-__release_date__= datetime.strptime('2023-07-04','%Y-%m-%d')
+__release_date__= datetime.strptime('2023-07-24','%Y-%m-%d')
