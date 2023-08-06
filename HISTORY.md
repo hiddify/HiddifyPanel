b24a092f22efa55822fc42abb0890e7ae83e4542
hiddify <hiddify@gmail.com>
2023-08-06 23:35:14 +0200

    release: version 8.0.0.dev21 🚀

--- HISTORY.md
+++ HISTORY.md
@@ -25,6 +25,8 @@
 
 #### Changes
 
+* Better singbox config. 
+
 * Handle exception for redis. 
 
 * Disable ui download. 
--- hiddifypanel/VERSION.py
+++ hiddifypanel/VERSION.py
@@ -1,3 +1,3 @@
-__version__='8.0.0.dev20'
+__version__='8.0.0.dev21'
 from datetime import datetime
 __release_date__= datetime.strptime('2023-08-06','%Y-%m-%d')
