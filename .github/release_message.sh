29b2810f06ebe4ed36587195fd08518f937eca17
hiddify <hiddify@gmail.com>
2023-08-10 10:08:42 +0200

    fix: release message bug

--- .github/release_message.sh
+++ .github/release_message.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 #previous_release=$(curl --silent "https://api.github.com/repos/hiddify/HiddifyPanel/releases/latest" | grep -Po '"tag_name": "\K.*?(?=")')
-pip install lastversion >/dev/null 2>&1
+pip install lastversion gitchangelog pystache >/dev/null 2>&1
 previous_release=$(lastversion --at pip hiddifypanel)
 current=$(cat hiddifypanel/VERSION)
 gitchangelog "${previous_release}..$current" ||echo "Error in change log"
