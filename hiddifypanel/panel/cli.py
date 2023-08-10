9f762db493590da6a0037c0e17199f0dd512abd3
hiddify <hiddify@gmail.com>
2023-08-10 20:51:49 +0200

    chg: remove access by https://ip if there is some domains

--- hiddifypanel/panel/cli.py
+++ hiddifypanel/panel/cli.py
@@ -89,7 +89,9 @@ def admin_links():
 
     domains = get_panel_domains()
     admin_links += f"Secure:\n"
-    admin_links += f"   (not signed) https://{server_ip}/{proxy_path}/{admin_secret}/admin/\n"
+    if not any([d for d in domains if 'sslip.io' not in d]):
+        admin_links += f"   (not signed) https://{server_ip}/{proxy_path}/{admin_secret}/admin/\n"
+
     # domains=[*domains,f'{server_ip}.sslip.io']
     for d in domains:
         admin_links += f"   https://{d.domain}/{proxy_path}/{admin_secret}/admin/\n"
