1140cc06ec81daf6579339f43aaffe9abd90de00
hiddify <hiddify@gmail.com>
2023-08-28 22:09:59 +0200

    new: disable cache on error

--- hiddifypanel/cache.py
+++ hiddifypanel/cache.py
@@ -1,11 +1,54 @@
 from redis_cache import RedisCache
+from functools import wraps
 import redis
 from pickle import dumps, loads
 redis_client = redis.from_url('unix:///opt/hiddify-config/other/redis/run.sock?db=0')
 
 
-# def exception_handler(**kwargs):
-#     pass
+def exception_handler(e, original_fn, args, kwargs):
+    return original_fn(*args, **kwargs)
+    pass
+
 
 # cache = RedisCache(redis_client=redis_client, exception_handler=exception_handler)
+# cache = RedisCache(redis_client=redis_client, prefix="h", serializer=dumps, deserializer=loads, exception_handler=exception_handler)
 cache = RedisCache(redis_client=redis_client, prefix="h", serializer=dumps, deserializer=loads)
+
+
+class CacheDecorator:
+    def __init__(self, *args, **kwargs):
+        pass
+
+    def __call__(self, fn):
+        @wraps(fn)
+        def inner(*args, **kwargs):
+
+            parsed_result = fn(*args, **kwargs)
+
+            return parsed_result
+
+        inner.invalidate = self.invalidate
+        inner.invalidate_all = self.invalidate_all
+        inner.instance = self
+        return inner
+
+    def invalidate(self, *args, **kwargs):
+        pass
+
+    def invalidate_all(self, *args, **kwargs):
+        pass
+
+
+class DisableCache:
+    cache = CacheDecorator
+
+
+# cache = DisableCache()
+try:
+    @cache.cache()
+    def test():
+        return 1
+    print("caching is working...", test())
+except Exception as e:
+    print('Caching Error! Disabling cache', e)
+    cache = DisableCache()
