b5b19a2cb35de1f5b42c0900118fb3fd1c44f594
hiddify <hiddify@gmail.com>
2023-09-14 13:08:51 +0200

    update tuic

--- hiddifypanel/panel/user/link_maker.py
+++ hiddifypanel/panel/user/link_maker.py
@@ -279,7 +279,7 @@ def to_link(proxy):
         if proxy['proto'] == 'v2ray':
             return f'{baseurl}?plugin=v2ray-plugin%3Bmode%3Dwebsocket%3Bpath%3D{proxy["path"]}%3Bhost%3D{proxy["host"]}%3Btls%3Budp-over-tcp=true#{name_link}'
     if proxy['proto'] == 'tuic':
-        baseurl = f'tuic://{proxy["uuid"]}:{proxy["uuid"]}@{proxy["server"]}:{proxy["port"]}?congestion_control=bbr&udp_relay_mode=native&sni={proxy["sni"]}'
+        baseurl = f'tuic://{proxy["uuid"]}:{proxy["uuid"]}@{proxy["server"]}:{proxy["port"]}?congestion_control=cubic&udp_relay_mode=native&sni={proxy["sni"]}'
         if proxy['mode'] == 'Fake' or proxy['allow_insecure']:
             baseurl += "&allow_insecure=1"
         return f"{baseurl}#{name_link}"
@@ -362,7 +362,7 @@ def to_clash(proxy, meta_or_normal):
         base["reduce-rtt"] = True
         base["request-timeout"] = 8000
         base["udp-relay-mode"] = 'native'
-        base["congestion-controller"] = 'bbr'
+        base["congestion-controller"] = 'cubic'
         base['sni'] = proxy['sni']
         return base
     elif proxy["proto"] in ["ss", "v2ray"]:
@@ -536,7 +536,7 @@ def to_singbox(proxy):
 
 
 def add_tuic(base, proxy):
-    base['congestion_control'] = "bbr"
+    base['congestion_control'] = "cubic"
     base['udp_relay_mode'] = 'native'
     base['zero_rtt_handshake'] = True
     base['heartbeat'] = "10s"
