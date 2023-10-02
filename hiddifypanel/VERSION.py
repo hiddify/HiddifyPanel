314dd6f54b2b25dd2624c4fad7f03784d9dfc006
hiddify <hiddify@gmail.com>
2023-10-02 10:35:57 +0200

    release: version 8.6.2 🚀

--- hiddifypanel/VERSION.py
+++ hiddifypanel/VERSION.py
@@ -1,3 +1,3 @@
-__version__='8.6.1'
+__version__='8.6.2'
 from datetime import datetime
-__release_date__= datetime.strptime('2023-09-13','%Y-%m-%d')
+__release_date__= datetime.strptime('2023-10-02','%Y-%m-%d')
