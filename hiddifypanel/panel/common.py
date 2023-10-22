e61b19ada668cf67ae2e98fa92bf2953b679d399
hiddify <hiddify@gmail.com>
2023-10-22 09:51:49 +0200

    fix: telegram admin bot bug

--- hiddifypanel/panel/common.py
+++ hiddifypanel/panel/common.py
@@ -17,6 +17,7 @@ def init_app(app):
     app.jinja_env.globals['DomainType'] = DomainType
     app.jinja_env.globals['UserMode'] = UserMode
     app.jinja_env.globals['hconfig'] = hconfig
+    app.jinja_env.globals['g'] = g
 
     @app.errorhandler(Exception)
     def internal_server_error(e):
