2fef7d77d5591a3c38937b49f37c52c7f99bde54
hiddify <hiddify@gmail.com>
2023-10-22 13:35:49 +0200

    fix: grpc h2 bug

--- hiddifypanel/panel/user/link_maker.py
+++ hiddifypanel/panel/user/link_maker.py
@@ -95,7 +95,7 @@ def make_proxy(proxy: Proxy, domain_db: Domain, phttp=80, ptls=443, pport=None):
     if 'reality' in proxy.l3:
         alpn = "h2" if proxy.transport in ['h2'] else 'http/1.1'
     else:
-        alpn = "h2" if proxy.l3 in ['tls_h2'] else 'h2,http/1.1' if proxy.l3 == 'tls_h2_h1' else "http/1.1"
+        alpn = "h2" if proxy.l3 in ['tls_h2', 'grpc'] else 'h2,http/1.1' if proxy.l3 == 'tls_h2_h1' else "http/1.1"
 
     if domain_db.mode != DomainType.old_xtls_direct and "tls" in proxy.l3 and proxy.cdn == ProxyCDN.direct and proxy.transport in [ProxyTransport.tcp, ProxyTransport.XTLS]:
         return {'name': name, 'msg': "only  old_xtls_direct  support this", 'type': 'debug', 'proto': proxy.proto}
