6211a9377865689750774022e311b8dfbf2cc4b0
hiddify <hiddify@gmail.com>
2023-11-05 01:07:28 +0100

    update

--- hiddifypanel/panel/commercial/restapi/v2/user/info.py
+++ hiddifypanel/panel/commercial/restapi/v2/user/info.py
@@ -6,29 +6,41 @@ from flask.views import MethodView
 
 from flask import current_app as app
 from apiflask import APIFlask, Schema, abort
-from apiflask.fields import Integer, String
+from apiflask.fields import Integer, String, Float, URL
 from apiflask.validators import Length, OneOf
 
 
-class PetIn(Schema):
-    name = String(required=True, validate=Length(0, 10))
-    category = String(required=True, validate=OneOf(['dog', 'cat']))
+class ProfileDTO(Schema):
+    profile_title = String(required=True)
+    profile_url = URL(required=True)
+    profile_usage_current = Float(required=True)
+    profile_usage_total = Float(required=True)
+    profile_remaining_days = Integer(required=True)
+    profile_reset_days = Integer()
+    telegram_bot_url = String()
+    admin_message_html = String()
+    admin_message_url = URL()
+    brand_title = String()
+    brand_icon_url = URL()
+    doh = URL()
+    def_lang = String(validate=OneOf(['en', 'fa', 'ru', 'pt', 'zh']))
 
 
-class PetOut(Schema):
-    id = Integer()
-    name = String()
-    category = String()
+class UserInfoChangableDTO(Schema):
+    language = String(required=True, validate=OneOf(['en', 'fa', 'ru', 'pt', 'zh']))
 
 
 class Info(MethodView):
     decorators = [hiddify.user_auth]
-    @app.output(PetOut)
+
+    @app.output(ProfileDTO)
     def get(self):
+        return g.uuid
+        # TODO use data from user panel page
         abort(404)
         pass
 
-    @app.input(PetIn(partial=True))
-    @app.output(PetOut)
+    @app.input(UserInfoChangableDTO)
     def post(self):
-        pass
+        # TODO add prefered language
+        return {'message': 'ok'}
