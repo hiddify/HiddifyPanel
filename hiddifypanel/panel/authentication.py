0a177000914d0a95e906ee7e496f1dea95c15d76
Sarina <iam54r1n40@gmail.com>
2023-12-11 12:09:40 +0330

    add: athentication with session

--- hiddifypanel/panel/authentication.py
+++ hiddifypanel/panel/authentication.py
@@ -1,6 +1,7 @@
 from apiflask import HTTPBasicAuth, HTTPTokenAuth
-from hiddifypanel.models.user import User, get_user_by_username, get_user_by_uuid
-from hiddifypanel.models.admin import AdminUser, get_admin_by_username, get_admin_by_uuid
+from hiddifypanel.models.user import User, get_user_by_uuid
+from hiddifypanel.models.admin import AdminUser, get_admin_by_uuid
+from flask import session
 
 basic_auth = HTTPBasicAuth()
 api_auth = HTTPTokenAuth("ApiKey")
@@ -26,12 +27,25 @@ def get_user_role(user) -> str | None:
 
 @basic_auth.verify_password
 def verify_basic_auth_password(username, password) -> str | None:
+    if session.get('account'):
+        if session['account']['role'] == 'user':
+            return get_user_by_uuid(session['account']['uuid'])
+        else:
+            return get_admin_by_uuid(session['account']['uuid'])
+
     username = username.strip()
     password = password.strip()
-    user = User.query.filter(User.username == username, User.password == password).first()
-    if user:
-        return user
-    return AdminUser.query.filter(AdminUser.username == username, AdminUser.password == password).first()
+    res = User.query.filter(User.username == username, User.password == password).first()
+    if not res:
+        res = AdminUser.query.filter(AdminUser.username == username, AdminUser.password == password).first()
+
+    if res:
+        session['account'] = {
+            'uuid': res.uuid,
+            'role': get_user_role(res),
+            # 'username': res.username,
+        }
+    return res
 
 
 @api_auth.verify_token
