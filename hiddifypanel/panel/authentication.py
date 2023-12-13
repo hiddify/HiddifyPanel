9c3dd7e97b2e4023db34fff370dd5848c9488906
Sarina <iam54r1n40@gmail.com>
2023-12-13 14:18:38 +0330

    chg: if client sent user/pass we try to authenticate with the user/pass, THEN we check session cookies

--- hiddifypanel/panel/authentication.py
+++ hiddifypanel/panel/authentication.py
@@ -3,7 +3,6 @@ from apiflask import HTTPBasicAuth, HTTPTokenAuth
 from hiddifypanel.models.user import User, get_user_by_uuid
 from hiddifypanel.models.admin import AdminUser, get_admin_by_uuid
 from flask import session
-from enum import auto
 from strenum import StrEnum
 
 basic_auth = HTTPBasicAuth()
@@ -66,31 +65,32 @@ def get_user_role(user) -> AccountRole | None:
 
 @basic_auth.verify_password
 def verify_basic_auth_password(username, password, check_session=True, roles: Set[AccountRole] = set()) -> AdminUser | User | None:
+    username = username.strip()
+    password = password.strip()
+    if username and password:
+        account = None
+        if roles:
+            if AccountRole.user in roles:
+                account = User.query.filter(User.username == username, User.password == password).first()
+            else:
+                account = AdminUser.query.filter(AdminUser.username == username, AdminUser.password == password).first()
+        else:
+            account = User.query.filter(User.username == username, User.password == password).first()
+            if not account:
+                account = AdminUser.query.filter(AdminUser.username == username, AdminUser.password == password).first()
+
+        if account:
+            if roles:
+                if account.mode in roles:
+                    set_authentication_in_session(account)
+                    return account
+            else:
+                set_authentication_in_session(account)
+                return account
+
     if check_session:
         account = verify_from_session(roles)
         if account:
-            return account
-
-    username = username.strip()
-    password = password.strip()
-    account = None
-    if roles:
-        if AccountRole.user in roles:
-            account = User.query.filter(User.username == username, User.password == password).first()
-        else:
-            account = AdminUser.query.filter(AdminUser.username == username, AdminUser.password == password).first()
-    else:
-        account = User.query.filter(User.username == username, User.password == password).first()
-        if not account:
-            account = AdminUser.query.filter(AdminUser.username == username, AdminUser.password == password).first()
-
-    if account:
-        if roles:
-            if account.mode in roles:
-                set_authentication_in_session(account)
-                return account
-        else:
-            set_authentication_in_session(account)
             return account
 
 
@@ -103,14 +103,15 @@ def verify_api_auth_token(token) -> User | AdminUser | None:
 
 def standalone_verify(roles: Set[AccountRole]) -> bool:
     '''This funciton is for ModelView-based views authentication'''
-    if verify_from_session(roles):
-        return True
 
     auth = basic_auth.get_auth()
     if auth:
         account = verify_basic_auth_password(auth.username, auth.password, check_session=False, roles=roles)
         if account:
             return True
+
+    if verify_from_session(roles):
+        return True
     return False
 
 # ADD ERROR HANDLING TO AUTHENTICATIONS
