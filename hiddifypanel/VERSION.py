f13acd24db17219ab197c29403c596a2e76d6e74
hiddify <hiddify@gmail.com>
2024-01-01 20:39:22 +0100

    update version

--- hiddifypanel/VERSION.py
+++ hiddifypanel/VERSION.py
@@ -1,3 +1,3 @@
 from datetime import datetime
 __version__ = 'X.X.X.devX'
-__release_date__ = datetime.strptime('2023-12-02', '%Y-%m-%d')
+__release_date__ = datetime.strptime('2024-01-01', '%Y-%m-%d')
