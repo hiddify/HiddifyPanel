f4cec0fa93b1d45440307806ba43f24b80840230
Sarina <iam54r1n40@proton.me>
2024-01-02 00:53:14 +0330

    chg: refactor parent domain model

--- hiddifypanel/models/__init__.py
+++ hiddifypanel/models/__init__.py
@@ -2,7 +2,7 @@ from .role import Role, AccountType
 from .config_enum import ConfigCategory, ConfigEnum, Lang
 from .config import StrConfig, BoolConfig, get_hconfigs, hconfig, set_hconfig, add_or_update_config, bulk_register_configs
 
-from .parent_domain import ParentDomain, add_or_update_parent_domains, bulk_register_parent_domains
+from .parent_domain import ParentDomain
 from .domain import Domain, DomainType, ShowDomain, get_domain, get_current_proxy_domains, get_panel_domains, get_proxy_domains, get_proxy_domains_db, get_hdomains, hdomain, add_or_update_domain, bulk_register_domains
 from .proxy import Proxy, ProxyL3, ProxyCDN, ProxyProto, ProxyTransport, add_or_update_proxy, bulk_register_proxies
 from .user import User, UserMode, remove_user, UserDetail, ONE_GIG
--- hiddifypanel/models/parent_domain.py
+++ hiddifypanel/models/parent_domain.py
@@ -19,33 +19,33 @@ class ParentDomain(db.Model, SerializerMixin):
                                    backref=backref('parent_domains', lazy='dynamic')
                                    )
 
-    def to_dict(d):
+    def to_dict(self):
         return {
-            'domain': d.domain,
-            'alias': d.alias,
-            'show_domains': [dd.domain for dd in d.show_domains]
+            'domain': self.domain,
+            'alias': self.alias,
+            'show_domains': [dd.domain for dd in self.show_domains]
         }
 
+    @staticmethod
+    def add_or_update(commit=True, **parent_domain):
+        dbdomain = ParentDomain.query.filter(ParentDomain.domain == parent_domain['domain']).first()
+        if not dbdomain:
+            dbdomain = ParentDomain(domain=parent_domain['domain'])
+            db.session.add(dbdomain)
+        show_domains = parent_domain.get('show_domains', [])
+        dbdomain.show_domains = Domain.query.filter(Domain.domain.in_(show_domains)).all()
+        dbdomain.alias = parent_domain.get('alias')
+        if commit:
+            db.session.commit()
 
-def add_or_update_parent_domains(commit=True, **parent_domain):
-    dbdomain = ParentDomain.query.filter(ParentDomain.domain == parent_domain['domain']).first()
-    if not dbdomain:
-        dbdomain = ParentDomain(domain=parent_domain['domain'])
-        db.session.add(dbdomain)
-    show_domains = parent_domain.get('show_domains', [])
-    dbdomain.show_domains = Domain.query.filter(Domain.domain.in_(show_domains)).all()
-    dbdomain.alias = parent_domain.get('alias')
-    if commit:
-        db.session.commit()
-
-
-def bulk_register_parent_domains(parent_domains, commit=True, remove=False):
-    for p in parent_domains:
-        add_or_update_parent_domains(commit=False, **p)
-    if remove:
-        dd = {p.domain: 1 for p in parent_domains}
-        for d in ParentDomain.query.all():
-            if d.domain not in dd:
-                db.session.delete(d)
-    if commit:
-        db.session.commit()
+    @staticmethod
+    def bulk_register(parent_domains, commit=True, remove=False):
+        for p in parent_domains:
+            ParentDomain.add_or_update(commit=False, **p)
+        if remove:
+            dd = {p.domain: 1 for p in parent_domains}
+            for d in ParentDomain.query.all():
+                if d.domain not in dd:
+                    db.session.delete(d)
+        if commit:
+            db.session.commit()
