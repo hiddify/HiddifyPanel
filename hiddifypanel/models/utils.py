edf2e4c993c3c700160e142debb1e3ff037043c2
hiddify <hiddify@gmail.com>
2024-01-10 15:58:59 +0100

    fix: bug in fill_username

--- hiddifypanel/models/utils.py
+++ hiddifypanel/models/utils.py
@@ -5,7 +5,7 @@ from slugify import slugify
 def fill_username(model) -> None:
     if model.username:
         return
-
+    model.username = model.name or ''
     minimum_username_length = 10
 
     if len(model.username) < minimum_username_length:
@@ -17,10 +17,10 @@ def fill_username(model) -> None:
 
     while not model.is_username_unique():
         rand_str = hutils.utils.get_random_string(2, 4)
-        model.username = generate_username(base_username, len(rand_str))
+        model.username += rand_str
 
 
 def fill_password(model) -> None:
     # TODO: hash the password
     if not model.password or len(model.password) < 16:
-        model.password = hutils.utils.get_random_password(length=16)
+        model.password=hutils.utils.get_random_password(length = 16)
