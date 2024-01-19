1a258674615192b18a49bb548481ca22f9aaec40
Sarina <iam54r1n40@proton.me>
2024-01-19 13:35:07 +0330

    chg: hide telegram id in add/edit page

--- hiddifypanel/panel/admin/UserAdmin.py
+++ hiddifypanel/panel/admin/UserAdmin.py
@@ -30,7 +30,7 @@ class UserAdmin(AdminLTEModelView):
     list_template = 'model/user_list.html'
 
     # form_columns = ["name", "current_usage_GB", "remaining_days", "comment", 'last_online', 'mode', 'admin', "uuid"]
-    form_excluded_columns = ['current_usage', 'monthly', 'last_online', 'expiry_time', 'last_reset_time', 'current_usage_GB',
+    form_excluded_columns = ['current_usage', 'monthly', 'telegram_id', 'last_online', 'expiry_time', 'last_reset_time', 'current_usage_GB',
                              'start_date', 'added_by', 'admin', 'details', 'max_ips', 'ed25519_private_key', 'ed25519_public_key', 'username', 'password']
     page_size = 50
     # edit_modal=True
@@ -251,8 +251,8 @@ class UserAdmin(AdminLTEModelView):
             raise ValidationError('Invalid UUID e.g.,' + str(uuid.uuid4()))
         if form.reset_usage.data:
             model.current_usage_GB = 0
-        if model.telegram_id and model.telegram_id != '0' and not re.match(r"^[1-9]\d*$", model.telegram_id):
-            raise ValidationError('Invalid Telegram ID')
+        # if model.telegram_id and model.telegram_id != '0' and not re.match(r"^[1-9]\d*$", model.telegram_id):
+        #     raise ValidationError('Invalid Telegram ID')
         # if form.disable_user.data:
         #     model.mode=UserMode.disable
         if form.reset_days.data:
