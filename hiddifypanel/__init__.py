0616bec768e76c61c1a3309196c2e8bc33f4b2d3
hiddify <hiddify@gmail.com>
2024-02-01 13:39:56 +0100

    new: optimize panel

--- hiddifypanel/__init__.py
+++ hiddifypanel/__init__.py
@@ -6,6 +6,6 @@ from .base import create_app, create_app_wsgi
 # from . import panel
 
 
-__all__ = ["create_app", "create_app_wsgi", "create_cli_app"]
+__all__ = ["create_app", "create_app_wsgi"]
 
 # application = create_app_wsgi()
