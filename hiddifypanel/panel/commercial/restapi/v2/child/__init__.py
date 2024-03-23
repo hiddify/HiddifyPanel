8be40678e97b98d7bb3ab2050d2f4ecd1b1cad64
Sarina <iam54r1n40@proton.me>
2024-03-23 08:34:02 +0330

    add: some apis to child panel named actions these apis allow the parent panel to control the child panel

--- hiddifypanel/panel/commercial/restapi/v2/child/__init__.py
+++ hiddifypanel/panel/commercial/restapi/v2/child/__init__.py
@@ -6,5 +6,11 @@ bp = APIBlueprint("api_child", __name__, url_prefix="/<proxy_path>/api/v2/child/
 def init_app(app):
     with app.app_context():
         from .register_parent import RegisterParent
+        from .actions import ApplyConfig, Restart, Status, UpdateUsage, Install
         bp.add_url_rule('/register-parent/', view_func=RegisterParent)
+        bp.add_url_rule('/status/', view_func=Status)
+        bp.add_url_rule('/restart/', view_func=Restart)
+        bp.add_url_rule('/apply-config/', view_func=ApplyConfig)
+        bp.add_url_rule('/install/', view_func=Install)
+        bp.add_url_rule('/update-usage/', view_func=UpdateUsage)
     app.register_blueprint(bp)
---
+++ hiddifypanel/panel/commercial/restapi/v2/child/actions.py
@@ -0,0 +1,54 @@
+from apiflask import abort, fields, Schema
+from flask import current_app as app
+from flask.views import MethodView
+
+from hiddifypanel.panel.run_commander import commander, Command
+from hiddifypanel.auth import login_required
+
+
+class Status(MethodView):
+    decorators = [login_required(parent_auth=True)]
+
+    def post(self):
+        commander(Command.status)
+        return {'status': 200, 'msg': 'ok'}
+
+
+class UpdateUsage(MethodView):
+    decorators = [login_required(parent_auth=True)]
+
+    def post(self):
+        commander(Command.update_usage)
+        return {'status': 200, 'msg': 'ok'}
+
+
+class Restart(MethodView):
+    decorators = [login_required(parent_auth=True)]
+
+    def post(self):
+        commander(Command.restart_services)
+        return {'status': 200, 'msg': 'ok'}
+
+
+class ApplyConfig(MethodView):
+    decorators = [login_required(parent_auth=True)]
+
+    def post(self):
+        commander(Command.apply)
+        return {'status': 200, 'msg': 'ok'}
+
+
+class InstallSchema(Schema):
+    full = fields.Boolean(description="full install", required=True, default=True)
+
+
+class Install(MethodView):
+    decorators = [login_required(parent_auth=True)]
+
+    @app.input(InstallSchema, arg_name="data")  # type: ignore
+    def post(self, data: dict):
+        if data.get('full'):
+            commander(Command.install)
+        else:
+            commander(Command.apply)
+        return {'status': 200, 'msg': 'ok'}
