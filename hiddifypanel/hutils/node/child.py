61b04c00983e26a3bbd1607de29f31e655f74c0d
Sarina <iam54r1n40@proton.me>
2024-03-27 11:46:53 +0330

    chg: refactor child.py

--- hiddifypanel/hutils/node/child.py
+++ hiddifypanel/hutils/node/child.py
@@ -12,12 +12,6 @@ from hiddifypanel.database import db
 # region private
 
 
-class __ApiDataType(StrEnum):
-    register = auto()
-    sync = auto()
-    usage = auto()
-
-
 def __send_put_request_to_parent(url: str, payload: dict, key: str) -> dict:
     res = requests.put(url, json=payload, headers={'Hiddify-API-Key': key}, timeout=10)
     if res.status_code != 200:
@@ -30,27 +24,26 @@ def __send_put_request_to_parent(url: str, payload: dict, key: str) -> dict:
     return res.json()
 
 
-def __get_panel_data_for_api(type: __ApiDataType) -> dict | List[dict]:
-    if type == __ApiDataType.register:
-        from hiddifypanel.panel.commercial.restapi.v2.parent.register_api import RegisterDataSchema
-        register_data = RegisterDataSchema()  # type: ignore
-        register_data.admin_users = [admin_user.to_schema() for admin_user in AdminUser.query.all()]  # type: ignore
-        register_data.users = [user.to_schema() for user in User.query.all()]  # type: ignore
-        register_data.domains = [domain.to_schema() for domain in Domain.query.all()]  # type: ignore
-        register_data.proxies = [proxy.to_schema() for proxy in Proxy.query.all()]  # type: ignore
-        register_data.hconfigs = [*[u.to_dict() for u in StrConfig.query.all()], *[u.to_dict() for u in BoolConfig.query.all()]]  # type: ignore
+def __get_register_data_for_api() -> dict:
+    from hiddifypanel.panel.commercial.restapi.v2.parent.register_api import RegisterDataSchema
+    register_data = RegisterDataSchema()  # type: ignore
+    register_data.admin_users = [admin_user.to_schema() for admin_user in AdminUser.query.all()]  # type: ignore
+    register_data.users = [user.to_schema() for user in User.query.all()]  # type: ignore
+    register_data.domains = [domain.to_schema() for domain in Domain.query.all()]  # type: ignore
+    register_data.proxies = [proxy.to_schema() for proxy in Proxy.query.all()]  # type: ignore
+    register_data.hconfigs = [*[u.to_dict() for u in StrConfig.query.all()], *[u.to_dict() for u in BoolConfig.query.all()]]  # type: ignore
 
-        return register_data.dump(register_data)  # type: ignore
-    elif type == __ApiDataType.sync:
-        from hiddifypanel.panel.commercial.restapi.v2.parent.sync_api import SyncSchema
-        sync_data = SyncSchema()  # type: ignore
-        sync_data.domains = [domain.to_schema() for domain in Domain.query.all()]  # type: ignore
-        sync_data.proxies = [proxy.to_schema() for proxy in Proxy.query.all()]  # type: ignore
-        sync_data.hconfigs = [*[u.to_dict() for u in StrConfig.query.all()], *[u.to_dict() for u in BoolConfig.query.all()]]  # type: ignore
+    return register_data.dump(register_data)  # type: ignore
 
-        return sync_data.dump(sync_data)  # type: ignore
-    else:
-        return hutils.node.get_users_usage_data_for_api()
+
+def __get_sync_data_for_api() -> dict:
+    from hiddifypanel.panel.commercial.restapi.v2.parent.sync_api import SyncSchema
+    sync_data = SyncSchema()  # type: ignore
+    sync_data.domains = [domain.to_schema() for domain in Domain.query.all()]  # type: ignore
+    sync_data.proxies = [proxy.to_schema() for proxy in Proxy.query.all()]  # type: ignore
+    sync_data.hconfigs = [*[u.to_dict() for u in StrConfig.query.all()], *[u.to_dict() for u in BoolConfig.query.all()]]  # type: ignore
+
+    return sync_data.dump(sync_data)  # type: ignore
 
 
 def __get_parent_panel_url() -> str:
@@ -93,7 +86,7 @@ def register_to_parent(name: str, mode: ChildMode = ChildMode.remote, set_db=Tru
         'unique_id': hconfig(ConfigEnum.unique_id),
         'name': name,
         'mode': mode,
-        'panel_data': __get_panel_data_for_api(__ApiDataType.register),
+        'panel_data': __get_register_data_for_api(),
     }
     p_key = hconfig(ConfigEnum.parent_admin_uuid)
     res = __send_put_request_to_parent(p_url, payload, p_key)
@@ -119,7 +112,7 @@ def sync_with_parent(set_db=True) -> bool:
     else:
         p_url += '/api/v2/parent/sync/'
 
-    payload = __get_panel_data_for_api(__ApiDataType.sync)
+    payload = __get_sync_data_for_api()
 
     res = __send_put_request_to_parent(p_url, payload, hconfig(ConfigEnum.unique_id))  # type: ignore
     if 'err' in res:
@@ -138,7 +131,7 @@ def sync_users_usage_with_parent(set_db=True) -> bool:
     else:
         p_url += '/api/v2/parent/usage/'
 
-    payload = __get_panel_data_for_api(__ApiDataType.usage)
+    payload = hutils.node.get_users_usage_data_for_api()
 
     res = __send_put_request_to_parent(p_url, payload, hconfig(ConfigEnum.unique_id))  # type: ignore
     if 'err' in res:
