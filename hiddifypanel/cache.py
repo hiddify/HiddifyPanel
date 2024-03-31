d6622cd15c0d96d7544fb047c83e9401e665e204
Sarina <iam54r1n40@proton.me>
2024-03-31 16:16:50 +0330

    chg: better redis cache invalidation

--- hiddifypanel/cache.py
+++ hiddifypanel/cache.py
@@ -1,4 +1,4 @@
-from redis_cache import RedisCache, CacheDecorator, compact_dump
+from redis_cache import RedisCache, CacheDecorator, compact_dump, chunks
 from redis_cache import loads as redis_cache_loads
 import redis
 from pickle import dumps, loads
@@ -7,42 +7,14 @@ from loguru import logger
 redis_client = redis.from_url('unix:///opt/hiddify-manager/other/redis/run.sock?db=0')
 
 
-class CustomCacheDecorator(CacheDecorator):
-
-    def __init__(self, *args, **kwargs):
-        self.redis_cache = kwargs.pop('redis_cache')
-        super().__init__(*args, **kwargs)
-
-    def __call__(self, fn):
-        new_fn = super().__call__(fn)
-        self.redis_cache.cached_functions.append(new_fn)
-        return new_fn
-
-
 class CustomRedisCache(RedisCache):
-    def __init__(self, redis_client, prefix="rc", serializer=compact_dump, deserializer=redis_cache_loads, key_serializer=None, exception_handler=None):
-        super().__init__(redis_client, prefix, serializer, deserializer, key_serializer, exception_handler)
-        self.cached_functions = []
-
-    def cache(self, ttl=0, limit=0, namespace=None, exception_handler=None):
-        return CustomCacheDecorator(
-            redis_client=self.client,
-            prefix=self.prefix,
-            serializer=self.serializer,
-            deserializer=self.deserializer,
-            key_serializer=self.key_serializer,
-            ttl=ttl,
-            limit=limit,
-            namespace=namespace,
-            exception_handler=exception_handler or self.exception_handler,
-            redis_cache=self
-        )
 
     def invalidate_all_cached_functions(self):
         try:
             logger.info("Invalidating all cached functions")
-            for cached_fn in self.cached_functions:
-                cached_fn.invalidate_all()
+            chunks_gen = chunks(f'{self.prefix}*', 500)
+            for keys in chunks_gen:
+                self.client.delete(*keys)
             logger.success("Successfully invalidated all cached functions")
             return True
         except Exception as err:
