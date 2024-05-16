38cc30455afaffb4504e85d6b5e688ca5df6c37b
Sarina <iam54r1n40@proton.me>
2024-05-16 17:02:41 +0330

    chg: refactor version checking

--- hiddifypanel/hutils/utils.py
+++ hiddifypanel/hutils/utils.py
@@ -1,9 +1,11 @@
 from flask_babel import lazy_gettext as _
 import requests
-
+from packaging.version import Version
 import re
 import sys
 
+from hiddifypanel.models.config import hconfig, ConfigEnum
+from hiddifypanel import __version__ as current_version
 from hiddifypanel.cache import cache
 
 
@@ -35,7 +37,48 @@ def get_latest_release_version(repo_name):
                 if ver == "latest":
                     return get_latest_release_version(repo_name.replace("-", ""))
                 return ver
-    except Exception as e:
-        return f'{e}'
+    except Exception:
+        return None
 
     return None
+
+
+def is_panel_outdated() -> bool:
+    pm = hconfig(ConfigEnum.package_mode)
+    try:
+        if pm == 'release':
+            if latest_v := get_latest_release_version('hiddifypanel'):
+                if compare_versions(latest_v, current_version) == 1:
+                    return True
+    except:
+        pass
+    return False
+
+
+def compare_versions(version_1: str, version_2: str) -> int:
+    """
+    Compare two version strings and return an integer based on their relative order.
+    Returns:
+        int:
+
+        - 1 if version_1 is greater than version_2.
+        - 0 if version_1 is equal to version_2.
+        - -1 if version_1 is less than version_2.
+
+    Examples:
+        >>> compare_versions("10.20.4", "10.20.4")
+        0
+        >>> compare_versions("10.20.4", "10.20.2")
+        1
+        >>> compare_versions("10.20.2", "10.20.4")
+        -1
+    """
+    v1 = Version(version_1)
+    v2 = Version(version_2)
+
+    if v1 > v2:
+        return 1  # version_1 is greater
+    elif v2 > v1:
+        return -1  # version_2 is greater
+    else:
+        return 0  # versions are equal
