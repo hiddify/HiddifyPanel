4b4ce981d12e52c7b0026f8d7cc27e3728b89df6
Sarina <iam54r1n40@proton.me>
2024-06-05 14:02:32 +0330

    chg: reduce complexity for reality domain validation

--- hiddifypanel/panel/admin/DomainAdmin.py
+++ hiddifypanel/panel/admin/DomainAdmin.py
@@ -233,19 +233,20 @@ class DomainAdmin(AdminLTEModelView):
                 for d in v.split(","):
                     if not d:
                         continue
-                    if not hutils.network.is_domain_reality_friendly(d):
+                    if not hutils.network.is_domain_reality_friendly(d):  # the minimum requirement for the REALITY protocol is to have tls1.3 and h2
                         raise ValidationError(_("Domain is not REALITY friendly!") + f' {d}')
 
                     if not hutils.network.is_in_same_asn(d, ipv4_list[0]):
-                        server_asn = hutils.network.get_ip_asn_name(ipv4_list[0])
-                        domain_asn = hutils.network.get_ip_asn_name(dip)  # type: ignore
+                        server_asn = hutils.network.get_ip_asn(ipv4_list[0])
+                        domain_asn = hutils.network.get_ip_asn(dip)  # type: ignore
                         msg = _("selected domain for REALITY is not in the same ASN. To better use of the protocol, it is better to find a domain in the same ASN.") + \
                             (f"<br> Server ASN={server_asn}<br>{d}_ASN={domain_asn}" if server_asn or domain_asn else "")
                         hutils.flask.flash(msg, 'warning')
 
             for d in model.servernames.split(","):
                 if not hutils.network.fallback_domain_compatible_with_servernames(model.domain, d):
-                    raise ValidationError(_("REALITY Fallback domain is not compaitble with server names!") + f' {d} != {model.domain}')
+                    msg = _("REALITY Fallback domain is not compaitble with server names!") + f' {d} != {model.domain}'
+                    hutils.flask.flash(msg, 'warning')
 
         if (model.cdn_ip):
             try:
