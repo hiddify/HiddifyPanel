96ec01ca898a9183495c8244a8ea220e3170a26a
hiddify-com <114227601+hiddify-com@users.noreply.github.com>
2024-06-30 14:57:59 +0330

    update

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 source /opt/hiddify-manager/common/utils.sh
 activate_python_venv
 while true; do
-    FLASK_RUN_HOST=0.0.0.0 FLASK_RUN_PORT=9000 python -m hiddifypanel run
+    FLASK_RUN_HOST=0.0.0.0 FLASK_RUN_PORT=9000 hiddify-panel-cli run
 done
