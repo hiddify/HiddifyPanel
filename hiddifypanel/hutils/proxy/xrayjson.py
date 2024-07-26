ada24bf44d466fa6a2068c9c6d6fbc450d4551f7
hiddify-com <114227601+hiddify-com@users.noreply.github.com>
2024-07-26 17:42:50 +0330

    change useragent of splithttp

--- hiddifypanel/hutils/proxy/xrayjson.py
+++ hiddifypanel/hutils/proxy/xrayjson.py
@@ -341,7 +341,9 @@ def add_splithttp_stream(ss: dict, proxy: dict):
     ss['splithttpSettings'] = {
         'path': proxy['path'],
         'host': proxy['host'],
-        # 'acceptProxyProtocol': '', for inbounds only
+        "headers": {
+            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/127.0.0.0 Safari/537.36"
+        }
     }
 
 
